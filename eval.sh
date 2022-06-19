1958306ac5ad5e49eeb5a80f268f24f0d8ddcc91
Hyoencheol <53032349+cjf8899@users.noreply.github.com>
2022-06-19 17:22:47 +0900

    upload remix code

---
+++ eval.sh
@@ -0,0 +1,1 @@
+python main.py --mode eval --num_domains 3 --w_hpf 0 --resume_iter 100000 --train_img_dir data/afhq/train  --val_img_dir data/afhq/val --checkpoint_dir expr/checkpoints_remix --eval_dir expr/eval_remix/afhq
